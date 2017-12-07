6ac3e0f777a13d468626dc9823de0709ec54eda8
Lena <lzhengem@gmail.com>
2017-12-06 16:14:38 -0800

    added displayShipOnBoard

--- Board.java
+++ Board.java
@@ -8,6 +8,10 @@ public class Board{
 
 	public static void main(String[] args){
 		Board board = new Board();
+		Point origin = new Point(0,0);
+		Point origin2 = new Point(2,2);
+		Ship ship = new Ship(origin,true,5);
+		Ship ship2 = new Ship(origin2,false,5);
 
 		// private Point ship1_origin = new Point(1,2);
 		// System.out.println(test_point);
@@ -15,9 +19,11 @@ public class Board{
 
 		// private Ship ship1 = new Ship(ship1_origin,true,3);
 		board.displayBoard();
-		board.layout[0][1] = "X";
-		board.layout[1][2] = "O";
-		board.displayBoard();
+		// board.layout[0][1] = "X";
+		// board.layout[1][2] = "O";
+		// board.displayBoard();
+		board.displayShipOnBoard(ship);
+		board.displayShipOnBoard(ship2);
 	}
 	public Board(){
 		// initialize everything on the board to '~'
@@ -49,4 +55,32 @@ public class Board{
 		}
 		System.out.println();
 	}
+public void displayShipOnBoard(Ship ship){
+		// display everything on the board
+		for(int row=layout.length-1; row >= 0 ; row--){
+		// for(int row=0; row <layout.length ; row++){
+			//displays the row numbers
+			System.out.print(row + " ");
+			for(int column=0; column < layout[0].length ; column++){
+				Point currentPoint = new Point(column, row);
+				//displays the coordinates
+				for(Point points : ship.get_coordinates()){
+					if (points.equals(currentPoint))
+						layout[column][row] = "X";
+
+				}
+				
+					System.out.print(layout[column][row] + " ");
+			}
+			System.out.println();
+		}
+
+		// display the column numbers
+		System.out.print("  ");
+		for(int column=0; column < layout.length ; column++){
+			System.out.print(column + " ");
+		}
+		System.out.println();
+	}
+
 }
--- Ship.java
+++ Ship.java
@@ -1,37 +1,63 @@
+import java.util.ArrayList;
 // A Ship class that stores the data for each ship. Your program will have five of these.
-// This ship class should keep two ArrayLists of points: one to store the points that the ship covers, and one to store the points that have been “hit” by the player.
+// This ship class should keep two ArrayLists of points: one to store the points that the ship covers, and one to store the points that have been "hit" by the player.
 public class Ship{
 	
-	ArrayList<Point> coordinates;
-	ArrayList<Point> hit;
+	ArrayList<Point> coordinates = new ArrayList<Point>();
+	ArrayList<Point> hit = new ArrayList<Point>();
 
 	//Point origin, boolean isVertical, int length
 	// this constructor accepts the origin or the ship (its lowest, or leftmost point), its length, and its orientation (vertical, or horizontal).
 	public Ship(Point origin, boolean isVertical, int length){
-		// add origin as first point
-		// loop through length  to add other points - if vertical, then add coordinates
-		// going up, if not vertical, add points going left
+		Point newPoint;
+		coordinates.add(origin);
+		// if the ship is vertical, add length to y axis
+		if(isVertical){
+			for (int y = 1; y < length; y++){
+				newPoint = new Point(origin.get_x(), origin.get_y() + y);
+				coordinates.add(newPoint);
+			}
+		}
+		// if ship is horizontal, add length to x axis
+		else{
+			for (int x = 1; x < length; x++){
+				newPoint = new Point(origin.get_x() + x, origin.get_y());
+				coordinates.add(newPoint);
+			}
+
+		}
+
+	}
+
+	public ArrayList<Point> get_coordinates(){
+		return coordinates;
 
 	}
 	// Returns true if a Ship covers a point on the board, false if it does not.
 	public boolean containsPoint(Point P){
+		boolean hasPoint = false;
+		for(Point point: coordinates){
+			if (point.equals(P))
+				hasPoint = true;
+		}
+		return hasPoint;
 
 	}
 	// Returns true if the receiving ship shares a point with the argument ship.
-	public boolean collidesWith(Ship s){
+	// public boolean collidesWith(Ship s){
 
-	}
-	//When the user enters a coordinate, this method can be called on each ship. If the ship contains the point, it should remember that it has been hit at that point. It could do this with a second PointCollection, or some other data strategy.
-	public void shotFiredAtPoint(Point p){
+	// }
+	// //When the user enters a coordinate, this method can be called on each ship. If the ship contains the point, it should remember that it has been hit at that point. It could do this with a second PointCollection, or some other data strategy.
+	// public void shotFiredAtPoint(Point p){
 
-	}
-	// returns true if shotFiredAtPoint has been called for this point in the ship. False if it has not, or if the point is not in the ship.
-	public boolean isHitAtPoint(Point p){
+	// }
+	// // returns true if shotFiredAtPoint has been called for this point in the ship. False if it has not, or if the point is not in the ship.
+	// public boolean isHitAtPoint(Point p){
 
-	}
-	//returns the number of points in the ship that have been hit. When the hitCount is equal to the length of the ship the ship is considered to be sunk.
-	public int hitCount(){
+	// }
+	// //returns the number of points in the ship that have been hit. When the hitCount is equal to the length of the ship the ship is considered to be sunk.
+	// public int hitCount(){
 
-	}
+	// }
 
 }
